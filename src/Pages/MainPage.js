d1a508d4a87a8f0d18944c78281a23d3efeb86b7
JuliCarracedo <julicarra@hotmail.com>
2022-05-16 15:14:37 -0300

    Add TrainerCard

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import RoutinesCard from '../RoutinesCard';
+import TrainerCard from '../TrainerCard';
 import UserCard from '../UserCard';
 
 const MainPage = () => (
   <div className="main-page">
     <UserCard />
     <RoutinesCard />
+    <TrainerCard />
   </div>
 );
 
