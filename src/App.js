181dd8ac0dd09f06b3a93e7b9ff7cd8b45b332e6
JuliCarracedo <julicarra@hotmail.com>
2022-05-16 14:34:09 -0300

    Create and add MainPage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
+import MainPage from './Pages/MainPage';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route />
+          <Route exact path='/' element={MainPage}/>
         </Routes>
       </Router>
     </div>
