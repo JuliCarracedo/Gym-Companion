0dd57c0d28516a7bfa84928c2438fb61afe3924a
JuliCarracedo <julicarra@hotmail.com>
2022-05-16 14:55:15 -0300

    Create and add RoutinesCard

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import RoutinesCard from '../RoutinesCard';
 import UserCard from '../UserCard';
 
 const MainPage = () => (
   <div className="main-page">
     <UserCard />
+    <RoutinesCard />
   </div>
 );
 
---
+++ src/RoutinesCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const RoutinesCard = () => {
+    return (
+        <div className="routines-card">
+            <h2>Routines</h2>
+            <object data="../public/dumbbell.svg"/>
+        </div>
+    )
+}
+
+export default RoutinesCard;
