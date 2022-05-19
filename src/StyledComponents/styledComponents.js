1468fbaab19fa44b6f747a4332580edc453fd439
JuliCarracedo <julicarra@hotmail.com>
2022-05-19 11:21:34 -0300

    Apply eslint changes

--- src/StyledComponents/styledComponents.js
+++ src/StyledComponents/styledComponents.js
@@ -1,11 +1,9 @@
 import styled from 'styled-components';
-import fondo from '../assets/fondo.png';
 
 const Wrapper = styled.div`
   color: whitesmoke;
-  padding: 1em;
   height: max-content;
-  background-image: url(${fondo});
+  background: #f4aa43;
   background-size: cover;
   display: flex;
   flex-direction: column;
@@ -16,20 +14,17 @@ const Wrapper = styled.div`
 
 export const BigCard = styled.div`
     padding: 1em;
-    margin: 1em 0;
-    background: #12ed12;
-    border-radius: 10px;
+    background: #12b69a;
     display: flex;
     flex-direction: column;
-    height: 30vh;
+    height: 40vh;
     justify-content: space-between;
+    margin-bottom: 0.5em;
 `;
 
 export const BigCardInner = styled.div`
     display: flex;
     flex-direction: row;
-    color: black;
-    font-size: 20px;
 `;
 export const BigCardButtons = styled.nav`
     display: flex;
@@ -38,22 +33,20 @@ export const BigCardButtons = styled.nav`
 `;
 
 export const PrimaryButton = styled.button`
-    font-size: 20px;
     padding: 1em;
     margin: 0.5em;
     color: whitesmoke;
-    background: #009933;
+    background: #166650;
     border-radius: 10px;
     border-width: 0;
     width: 35vw;
 `;
 
 export const SecondaryButton = styled.button`
-font-size: 20px;
     padding: 1em;
     margin: 0.5em;
     color: whitesmoke;
-    background: #6eb491;
+    background: #225441;
     border-radius: 10px;
     border-width: 0;
     width: 35vw;
@@ -61,9 +54,8 @@ font-size: 20px;
 
 export const SmallCard = styled.div`
     padding: 2em;
-    margin: 1em 0;
-    background: #6e3667;
-    border-radius: 10px;
+    margin: 0.5em 1em;
+    background: #d9610c;
     display: flex;
     flex-direction: row;
     height: 10vh;
