9e25c333ffb1065c4a3d7dc2e8ed3ada3cf4df52
JuliCarracedo <julicarra@hotmail.com>
2022-05-30 12:31:20 -0300

    apply eslint autofix

--- src/Pages/Routines.js
+++ src/Pages/Routines.js
@@ -1,21 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Routines = () => {
-    return (
-        <div>
-            <Link to="/routine">
-                <section>
-                    Routine 1
-                </section>
-            </Link>
-            <Link to="/routine">
-                <section>
-                    Routine 2
-                </section>
-            </Link>
-        </div>
-    )
-}
+const Routines = () => (
+  <div>
+    <Link to={`/routine/${1}`}>
+      <section>
+        Routine 1
+      </section>
+    </Link>
+    <Link to={`/routine/${2}`}>
+      <section>
+        Routine 2
+      </section>
+    </Link>
+  </div>
+);
 
-export default Routines
+export default Routines;
